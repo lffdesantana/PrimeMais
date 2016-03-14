56fb2ff98f70be44a31078ded4ca077ade22bb47
LUIS FERNANDO <lffdesantana@gmail.com>
2016-03-13 22:07:55 -0300

    Add validate uniqueness in field email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
     EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
     belongs_to :person
     enum role:[:normal_user, :admin]
-    validates :email,  presence: true
+    validates :person_id, :email,  presence: true
+    validates :email, uniqueness: true
 
     validates_format_of :email, with: EMAIL_REGEXP
     # Include default devise modules. Others available are:
